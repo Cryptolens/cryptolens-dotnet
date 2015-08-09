c2b8dbc88c91eea2330dcadc4bde9fe616ed7f44
artemlos <artem@artemlos.net>
2015-08-09 12:28:22 +0200

    Add Extension Methods class that was not added in previous commit.

---
+++ SKM/ExtensionMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SKGL
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsValid(this KeyInformation keyInformation )
+        {
+            if(keyInformation != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static KeyInformation HasNotExpired(this KeyInformation keyInformation)
+        {
+            if (keyInformation != null)
+            {
+                TimeSpan ts = keyInformation.ExpirationDate - DateTime.Today;
+
+                if (ts.Days >= 0)
+                {
+                    return keyInformation;
+                }
+            }
+            return null;
+        }
+    }
+}
