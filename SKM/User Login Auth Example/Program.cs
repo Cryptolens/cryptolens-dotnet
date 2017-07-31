e776be067af80966ffb48862eb6f81e22cd8b546
artemlos <artem@artemlos.net>
2017-07-31 15:40:57 +0200

    Working on example

--- SKM/User Login Auth Example/Program.cs
+++ SKM/User Login Auth Example/Program.cs
@@ -9,21 +9,36 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+using SKGL;
+
 namespace User_Login_Auth_Example
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var aaa = new RSACryptoServiceProvider(2048);
-            aaa.FromXmlString("<RSAKeyValue><Modulus>sGbvxwdlDbqFXOMlVUnAF5ew0t0WpPW7rFpI5jHQOFkht/326dvh7t74RYeMpjy357NljouhpTLA3a6idnn4j6c3jmPWBkjZndGsPL4Bqm+fwE48nKpGPjkj4q/yzT4tHXBTyvaBjA8bVoCTnu+LiC4XEaLZRThGzIn5KQXKCigg6tQRy0GXE13XYFVz/x1mjFbT9/7dS8p85n8BuwlY5JvuBIQkKhuCNFfrUxBWyu87CFnXWjIupCD2VO/GbxaCvzrRjLZjAngLCMtZbYBALksqGPgTUN7ZM24XbPWyLtKPaXF2i4XRR9u6eTj5BfnLbKAU5PIVfjIS+vNYYogteQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>");
 
-            //WyI0NzUiLCJ5aGQvNC9xNU80eEhyWld1UGZQN3d6TytMM0dUV2xrT1VBUlBKY3d6Il0=
-            var q = UserLoginAuth.GetLicenseKeys("test123", "WyI0NzUiLCJ5aGQvNC9xNU80eEhyWld1UGZQN3d6TytMM0dUV2xrT1VBUlBKY3d6Il0=", "Artem", 5, aaa.ExportParameters(false), null, new RSACryptoServiceProvider(2048));
+            // This is found at https://serialkeymanager.com/User/Security
+            var RSAPublicKey = new RSACryptoServiceProvider(2048);
+            RSAPublicKey.FromXmlString("<RSAKeyValue><Modulus>sGbvxwdlDbqFXOMlVUnAF5ew0t0WpPW7rFpI5jHQOFkht/326dvh7t74RYeMpjy357NljouhpTLA3a6idnn4j6c3jmPWBkjZndGsPL4Bqm+fwE48nKpGPjkj4q/yzT4tHXBTyvaBjA8bVoCTnu+LiC4XEaLZRThGzIn5KQXKCigg6tQRy0GXE13XYFVz/x1mjFbT9/7dS8p85n8BuwlY5JvuBIQkKhuCNFfrUxBWyu87CFnXWjIupCD2VO/GbxaCvzrRjLZjAngLCMtZbYBALksqGPgTUN7ZM24XbPWyLtKPaXF2i4XRR9u6eTj5BfnLbKAU5PIVfjIS+vNYYogteQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>");
 
-            //var q2 = UserLoginAuth.GetLicenseKeys("test123", "WyI0MzAiLCJvYmdWdk00WWlldXNWakdqakovTHhzRDFNenU2WklZbGU4NVBQYzEwIl0=", "Artem", aaa.ExportParameters(false), q.getLicenseKeyToken , new RSACryptoServiceProvider(2048));
+            var authRequest = UserLoginAuth.GetLicenseKeys(SKGL.SKM.getMachineCode(SKGL.SKM.getSHA256), "WyI0NzUiLCJ5aGQvNC9xNU80eEhyWld1UGZQN3d6TytMM0dUV2xrT1VBUlBKY3d6Il0=", "Artem", 5, RSAPublicKey.ExportParameters(false), null, new RSACryptoServiceProvider(2048));
 
-            var b = JsonConvert.DeserializeObject<GetLicenseKeysResult>(q.jsonResult);
+            if (authRequest.error == null)
+            {
+                var data = JsonConvert.DeserializeObject<GetLicenseKeysResult>(authRequest.jsonResult);
+
+                var licenses = JsonConvert.DeserializeObject<List<KeyInfoResult>>(System.Text.UTF8Encoding.UTF8.GetString(Convert.FromBase64String(data.ActivatedMachineCodes)));
+
+                licenses.Where(x => x.LicenseKey.ProductId == 2);
+
+            }
+            else
+            {
+                Console.WriteLine("An error occurred.");
+            }
+
+
         }
     }
 
