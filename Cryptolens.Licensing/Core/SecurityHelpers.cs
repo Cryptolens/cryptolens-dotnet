a40bcf3317a82a0708d7661c6b5a5253180c05a9
Artem Los <arteml@kth.se>
2018-10-02 10:17:14 +0200

    Fix #20

--- Cryptolens.Licensing/Core/SecurityHelpers.cs
+++ Cryptolens.Licensing/Core/SecurityHelpers.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Xml;
 
 namespace SKM.V3.Internal
 {
@@ -31,6 +32,41 @@ namespace SKM.V3.Internal
                 return null;
 
             return new VerificationResult { Object = UTF8Encoding.UTF8.GetString(dataBytes), SignDate = DateTimeOffset.FromUnixTimeSeconds(obj.Date).Date };
+        }
+#endif
+
+#if NETSTANDARD2_0
+
+        /// <summary>
+        /// Retrieves the public key parameters from XML string. Used for compatibility with .NET Framework ToXMLString.
+        /// </summary>
+        public static RSAParameters FromXMLString(string xmlPublicKey)
+        {
+            // inspired by https://gist.github.com/Jargon64/5b172c452827e15b21882f1d76a94be4/
+
+            RSAParameters parameters = new RSAParameters();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlPublicKey);
+
+            if (xmlDoc.DocumentElement.Name.Equals("RSAKeyValue"))
+            {
+                foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+                {
+                    switch (node.Name)
+                    {
+                        case "Modulus": parameters.Modulus = Convert.FromBase64String(node.InnerText); break;
+                        case "Exponent": parameters.Exponent = Convert.FromBase64String(node.InnerText); break;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid XML RSA key.");
+            }
+
+            return parameters;
+
         }
 #endif
     }
