2072bfb625e17e01efe242d2c33ddff95ddbc8af
Artem Los <arteml@kth.se>
2018-10-10 14:35:01 +0200

    Move payment form session creation from Experimental to Cryptolens.Licensing

--- Cryptolens.Licensing/WebAPIModels.cs
+++ Cryptolens.Licensing/WebAPIModels.cs
@@ -19,6 +19,29 @@ namespace SKM.V3.Models
 
     }
 
+    public class CreateSessionResult : BasicResult
+    {
+        public string SessionId { get; set; }
+    }
+    public class CreateSessionModel
+    {
+        public int PaymentFormId { get; set; }
+        public double Price { get; set; }
+        public string Currency { get; set; }
+
+        public string Heading { get; set; } // remember to set a max length.
+
+        public string ProductName { get; set; }
+
+        [MaxLength(1000)]
+        public string CustomField { get; set; }
+
+        [MaxLength(1000)]
+        public string Metadata { get; set; }
+
+        public long Expires { get; set; }
+    }
+
     public class GetMessagesResult : BasicResult
     {
         public List<MessageObject> Messages { get; set; }
