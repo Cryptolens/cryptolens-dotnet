4153c830222b807ba8a8a53c943a1ecadae9313c
Artem Los <arteml@kth.se>
2019-09-08 12:24:20 +0200

    Add tests

---
+++ SKM Test/V3/TestSubscription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SKM.V3;
+using SKM.V3.Models;
+using SKM.V3.Methods;
+
+namespace SKM_Test
+{
+    [TestClass]
+    public class TestSubscription
+    {
+        [TestMethod]
+        public void TestRecordUsage()
+        {
+            var auth = AccessToken.AccessToken.SubscriptionMethods;
+            var res = Subscription.RecordUsage(auth, new RecordUsageModel { Amount = 1, ProductId = 3349, Key = "CMXKC-GUQRW-EJUGS-RRPUR" });
+
+            Assert.IsTrue(res != null && res.Result == ResultType.Success);
+        }
+    }
+}
